e725fb3d96b9287a980548f65cc7e146c652d990
Akshat Kushwaha <akshat.official999@gmail.com>
2022-12-06 02:44:48 +0530

    test commit

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,81 +1,90 @@
 import axios from "axios";
 
-const url = "http://localhost:8000/api/";
+const api_key = "48d198c2c635e2c64caa41df6e4acda6";
 
-const BASE_URL = "https://api.themoviedb.org/3/";
+const BASE_URL = "https://api.themoviedb.org/3/movie/";
 
-const getMovies = async () => await axios.get(url + "movies");
+const getPopularMovies = async function (page) {
+  await axios.get(BASE_URL + "popular", {
+    params: {
+      api_key: api_key,
+      page: page,
+    },
+  });
+};
 
 const getMoviesGenre = async (genre) =>
-  await axios.get(url + "movies/genre/" + genre);
+  await axios.get(BASE_URL + "movies/genre/" + genre);
 
-const getMovie = async (id) => await axios.get(url + "movies/" + id);
+const getMovie = async (id) => await axios.get(BASE_URL + "movies/" + id);
 
 const getMovieReviews = async (id) =>
-  await axios.get(url + "movies/" + id + "/reviews");
+  await axios.get(BASE_URL + "movies/" + id + "/reviews");
 
 const getMovieCast = async (id) =>
-  await axios.get(url + "movies/" + id + "/cast");
+  await axios.get(BASE_URL + "movies/" + id + "/cast");
 
 const getMovieImages = async (id) =>
-  await axios.get(url + "movies/" + id + "/images");
+  await axios.get(BASE_URL + "movies/" + id + "/images");
 
 const getMovieRecommendations = async (id) =>
-  await axios.get(url + "movies/" + id + "/recommendations");
+  await axios.get(BASE_URL + "movies/" + id + "/recommendations");
 
 const getMovieSimilar = async (id) =>
-  await axios.get(url + "movies/" + id + "/similar");
+  await axios.get(BASE_URL + "movies/" + id + "/similar");
 
 const getMovieKeywords = async (id) =>
-  await axios.get(url + "movies/" + id + "/keywords");
+  await axios.get(BASE_URL + "movies/" + id + "/keywords");
 
 const getMovieWatchProviders = async (id) =>
-  await axios.get(url + "movies/" + id + "/watch/providers");
+  await axios.get(BASE_URL + "movies/" + id + "/watch/providers");
 
 const getMoviesYear = async (year) =>
-  await axios.get(url + "movies/year/" + year);
+  await axios.get(BASE_URL + "movies/year/" + year);
 
 const getMoviesRating = async (rating) =>
-  await axios.get(url + "movies/rating/" + rating);
+  await axios.get(BASE_URL + "movies/rating/" + rating);
 
 const getMoviesTitle = async (title) =>
-  await axios.get(url + "movies/title/" + title);
+  await axios.get(BASE_URL + "movies/title/" + title);
 
 const getMoviesDirector = async (director) =>
-  await axios.get(url + "movies/director/" + director);
+  await axios.get(BASE_URL + "movies/director/" + director);
 
 const getMoviesActor = async (actor) =>
-  await axios.get(url + "movies/actor/" + actor);
+  await axios.get(BASE_URL + "movies/actor/" + actor);
 
 const getMoviesCountry = async (country) =>
-  await axios.get(url + "movies/country/" + country);
+  await axios.get(BASE_URL + "movies/country/" + country);
 
 const getMoviesLanguage = async (language) =>
-  await axios.get(url + "movies/language/" + language);
+  await axios.get(BASE_URL + "movies/language/" + language);
 
 const getMoviesProduction = async (production) =>
-  await axios.get(url + "movies/production/" + production);
+  await axios.get(BASE_URL + "movies/production/" + production);
 
-const createMovie = async (movie) => await axios.post(url + "movies", movie);
+const createMovie = async (movie) =>
+  await axios.post(BASE_URL + "movies", movie);
 
 const updateMovie = async (id, movie) =>
-  await axios.put(url + "movies/" + id, movie);
+  await axios.put(BASE_URL + "movies/" + id, movie);
 
-const deleteMovie = async (id) => await axios.delete(url + "movies/" + id);
+const deleteMovie = async (id) => await axios.delete(BASE_URL + "movies/" + id);
 
-const getGenres = async () => await axios.get(url + "genres");
+const getGenres = async () => await axios.get(BASE_URL + "genres");
 
-const getGenre = async (id) => await axios.get(url + "genres/" + id);
+const getGenre = async (id) => await axios.get(BASE_URL + "genres/" + id);
 
-const createGenre = async (genre) => await axios.post(url + "genres", genre);
+const createGenre = async (genre) =>
+  await axios.post(BASE_URL + "genres", genre);
 
 const updateGenre = async (id, genre) =>
-  await axios.put(url + "genres/" + id, genre);
+  await axios.put(BASE_URL + "genres/" + id, genre);
 
-const deleteGenre = async (id) => await axios.delete(url + "genres/" + id);
+const deleteGenre = async (id) => await axios.delete(BASE_URL + "genres/" + id);
 
 export default {
-  getMovies,
+  getPopularMovies,
   getMoviesGenre,
   getMovie,
   getMovieReviews,
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
+import getPopularMovies from "../api/movies";
 
 export default class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: {},
+    };
+  }
+
+  componentDidMount() {
+    this.fetchPopularMovies();
+  }
+
+  fetchPopularMovies = async () => {
+    console.log("Fetching popular movies...");
+    try {
+      const response = await getPopularMovies(1);
+      console.log(response);
+      // this.setState({ movies: response.data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <>
         <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-4xl font-mono">Homepage</h1>
+          <h1 className="text-4xl font-mono">Welcome to the Movie Database!</h1>
         </div>
       </>
     );
