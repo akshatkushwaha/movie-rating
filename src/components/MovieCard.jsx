c4257d60bac3d0f29560fb7f82353ced964cef38
Akshat Kushwaha <akshat.official999@gmail.com>
2022-12-23 20:19:07 +0530

    Made Movie Detail Grid of 2 for mobile

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,9 +18,9 @@ export default function MovieCard(props) {
 
   return (
     <>
-      <div className="rounded overflow-hidden shadow-base-200 shadow-2xl m-4 transform transition duration-500 hover:scale-110 bg-base-100">
+      <div className="rounded overflow-hidden shadow-base-200 shadow-2xl transform transition duration-500 hover:scale-110 bg-base-100">
         <Link reloadDocument to={`/movie/${props.id}`}>
-          <div className="w-full rounded overflow-hidden h-96">
+          <div className="w-full rounded overflow-hidden">
             <img
               className="w-full"
               src={
@@ -32,26 +32,28 @@ export default function MovieCard(props) {
             />
           </div>
           <div className="py-2 px-4">
-            <h1 className="text-xl font-bold py-2 h-20">{props.title}</h1>
+            <h1 className="text-base md:text-xl font-bold pb-2">
+              {props.title}
+            </h1>
             <div className="text-neutral-content">
-              <span className="bg-neutral inline-block rounded-full px-3 py-1 mr-2 text-sm font-semibold">
+              <span className="bg-neutral inline-block rounded-full px-3 py-1 mr-2 text-xs md:text-sm font-normal md:font-semibold">
                 {props.vote_average}
               </span>
-              <span className="bg-neutral inline-block rounded-full px-3 py-1 text-sm font-semibold">
+              <span className="bg-neutral inline-block rounded-full px-3 py-1 text-xs md:text-sm font-normal md:font-semibold">
                 {props.release_date}
               </span>
             </div>
+            <div className="movie_card_genre my-2 flex flex-row overflow-x-scroll">
+              {props.genre_ids?.map((id) => (
+                <Link reloadDocument key={id} to={`/genre/${id}/1`}>
+                  <span className="bg-neutral text-neutral-content inline-block rounded-full px-2 py-1 mr-2 text-xs md:text-sm font-normal md:font-semibold">
+                    {props.genreDB?.find((genre) => genre.id === id)?.name}
+                  </span>
+                </Link>
+              ))}
+            </div>
           </div>
         </Link>
-        <div className="movie_card_genre my-2 px-2 flex flex-row overflow-x-scroll">
-          {props.genre_ids?.map((id) => (
-            <Link reloadDocument key={id} to={`/genre/${id}/1`}>
-              <span className="bg-neutral text-neutral-content inline-block rounded-full px-3 py-1 m-1 text-sm font-semibold">
-                {props.genreDB?.find((genre) => genre.id === id)?.name}
-              </span>
-            </Link>
-          ))}
-        </div>
       </div>
     </>
   );
