c087d797145ace7e08aec594f9bdf2e93b9d73ea
akshatkushwaha <akshat.official999@gmail.com>
2023-03-23 01:19:25 +0530

    Image popup loading spinner

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -34,6 +34,7 @@ export default function MovieDetails() {
   const [fullScreenImage, setFullScreenImage] = useState(false);
   const [fullScreenImageIndex, setFullScreenImageIndex] = useState(0);
   const [posterOrImage, setPosterOrImage] = useState("image");
+  const [fullScreenImageLoading, setFullScreenImageLoading] = useState(true);
 
   useEffect(() => {
     fetchMovie();
@@ -110,17 +111,43 @@ export default function MovieDetails() {
         {/* full screen movie Images popup */}
         {fullScreenImage && (
           <div className="z-40 fixed top-0 left-0 w-screen h-screen bg-gray-900 bg-opacity-80 flex flex-col items-center justify-center">
-            <div className="w-full h-full flex flex-row items-center justify-center py-4 px-4">
-              <button className="bg-gray-900 bg-opacity-80 text-gray-100 rounded-full p-2 mt-2">
+            <div className="w-full h-full flex flex-row items-center justify-center p-4">
+              <button>
                 <ArrowLeftIcon
-                  className="w-6 h-6"
+                  className="w-8 h-8 fixed left-2 md:left-10 bg-accent text-accent-content rounded-full p-2"
                   onClick={() => {
                     fullScreenImageIndex === 0
                       ? setFullScreenImageIndex(images.length - 1)
                       : setFullScreenImageIndex(fullScreenImageIndex - 1);
+
+                    setFullScreenImageLoading(true);
                   }}
                 />
               </button>
+              <div className={fullScreenImageLoading ? "block" : "hidden"}>
+                <div className="relative w-full h-full">
+                  <div className="w-full h-full flex flex-row items-center justify-center">
+                    <div role="status">
+                      <svg
+                        aria-hidden="true"
+                        className="mr-2 w-8 h-8 animate-spin fill-primary text-base-content"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <img
                 src={
                   posterOrImage === "image"
@@ -128,20 +155,26 @@ export default function MovieDetails() {
                     : `https://image.tmdb.org/t/p/original/${posters[fullScreenImageIndex].file_path}`
                 }
                 alt={movie.title}
-                className="h-full w-full object-contain"
+                onLoad={() => setFullScreenImageLoading(false)}
+                className={
+                  (fullScreenImageLoading ? "hidden" : "block") +
+                  " w-full h-full object-contain"
+                }
               />
-              <button className="bg-gray-900 bg-opacity-80 text-gray-100 rounded-full p-2 mt-2">
+              <button>
                 <ArrowRightIcon
-                  className="w-6 h-6"
+                  className="w-8 h-8 fixed right-2 md:right-10 bg-accent text-accent-content rounded-full p-2"
                   onClick={() => {
                     fullScreenImageIndex === images.length - 1
                       ? setFullScreenImageIndex(0)
                       : setFullScreenImageIndex(fullScreenImageIndex + 1);
+
+                    setFullScreenImageLoading(true);
                   }}
                 />
               </button>
               <button
-                className="fixed top-10 right-10 bg-gray-900 bg-opacity-80 text-gray-100 rounded-full p-2 mt-2"
+                className="fixed top-10 right-10 bg-gray-900 bg-opacity-80 text-gray-100 rounded-full p-2"
                 onClick={() => {
                   setFullScreenImage(false);
                   document.body.classList.remove("overflow-hidden");
@@ -219,7 +252,7 @@ export default function MovieDetails() {
                 <p className="text-lg py-4 text-justify">
                   {showFullOverview
                     ? movie.overview
-                    : movie.overview.substring(0, 600) + "..."}
+                    : movie.overview.substring(0, 300) + "..."}
                   <span
                     className="text-sm font-bold cursor-pointer"
                     onClick={() => setShowFullOverview(!showFullOverview)}
